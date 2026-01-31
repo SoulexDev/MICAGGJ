00b6614f5ec83bdfca2d48a82899fd7258ea9ccf
SoulexDev <fnafiplayar@gmail.com>
2026-01-31 18:35:32 -0500

    finished heuristics script

---
+++ Assets/Scripts/Entities/CharacterManager.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    public static CharacterManager Instance;
+    public List<Character> characters = new List<Character>();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public void AddCharacter(Character c)
+    {
+        if (!characters.Contains(c))
+            characters.Add(c);
+    }
+    public void RemoveCharacter(Character c)
+    {
+        if (characters.Contains(c))
+            characters.Remove(c);
+    }
+}
