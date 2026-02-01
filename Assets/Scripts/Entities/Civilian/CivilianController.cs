b14aba0c8faeb4fe825fa4246797edbdb90d5091
SoulexDev <fnafiplayar@gmail.com>
2026-01-31 20:17:28 -0500

    imported audio. character fixes

---
+++ Assets/Scripts/Entities/Civilian/CivilianController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum CivilianState { Idle, RunFrom, RunTo }
+public class CivilianController : StateMachine<CivilianController>
+{
+    public NavMeshAgent agent;
+    public Character characterData;
+
+    private void Awake()
+    {
+        stateDictionary.Add(CivilianState.Idle, new CivilianIdle());
+        stateDictionary.Add(CivilianState.RunFrom, new CivilianRunFrom());
+        stateDictionary.Add(CivilianState.RunTo, new CivilianRunTo());
+
+        SwitchState(CivilianState.Idle);
+    }
+}
